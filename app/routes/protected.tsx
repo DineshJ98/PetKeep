fd79e5761b0b6f0d5b9eea2781214f6f67dc76bd
Dinesh Jeewantha <jeewanthadinesh1@gmail.com>
2026-07-28 10:44:00 +0530

    restrict users from admin pages

--- app/routes/protected.tsx
+++ app/routes/protected.tsx
@@ -1,6 +1,13 @@
 import { serverSessionService } from "~/service/session.server";
 import { Outlet, redirect } from "react-router";
 import type { Route } from "./+types/protected";
+import { jwtDecode } from "jwt-decode";
+
+interface SpringBootJWTPayload {
+  sub: string;
+  roles: Array<{ authority: string }>;
+  exp: number;
+}
 
 export async function loader({ request }: Route.LoaderArgs) {
   const springCookie = await serverSessionService.getBackendCookie(request);
@@ -8,6 +15,28 @@ export async function loader({ request }: Route.LoaderArgs) {
   if (!springCookie) {
     return redirect("/");
   }
+
+  try {
+    const tokenString = springCookie.replace("jwt_token=", "").trim();
+    console.log(`trimmed token: ${tokenString}`);
+
+    const decodedToken = jwtDecode<SpringBootJWTPayload>(tokenString);
+    console.log(`decoded token: ${JSON.stringify(decodedToken)}`);
+
+    const userAuthority = decodedToken.roles[0]?.authority || "ROLE_USER";
+    console.log(`user authority: ${userAuthority}`);
+
+    const targetUrlPath = new URL(request.url).pathname;
+    console.log(`url path: ${targetUrlPath}`);
+
+    if (targetUrlPath.startsWith("/admin") && userAuthority !== "ROLE_ADMIN") {
+      console.log(`user access admin`);
+      return redirect("/");
+    }
+  } catch (error) {
+    return redirect("/");
+  }
+
   return null;
 }
 
